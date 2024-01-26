48ab713c38eee7986e3637d50fb0bc5cb2fcb740
adityamishra139 <aditya2vibha@gmail.com>
2024-01-26 19:14:10 +0530

    homepage added

--- main.dart
+++ main.dart
@@ -1,33 +1,71 @@
-import 'package:bloodbond_app/features/app/splash-screen/splash_screen.dart';
-import 'package:bloodbond_app/features/user_auth/presentation/pages/Login_Page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:bloodbond/homepage.dart';
 import 'package:flutter/material.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCfVsu_mBGAgRzM9PkiwLMb1slncdPt0qo",
-          appId: "1:837735829285:web:698e783895c0a7989ed4cc",
-          messagingSenderId: "837735829285",
-          projectId: "flutter-firebase-fb309"),
-    );
-  }
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'flutter demo',
-      home: SplashScreen(child: LoginPage()),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const RootPage(),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BloodBond"),
+        centerTitle: true,
+        
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: (){
+            
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person), // Add your action icon here
+            onPressed: () {
+              // Do something when the action icon is pressed
+            },
+          ),
+          
+        ],
+        
+      ),
+      body: const Homepage(),
+      
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "person"),
+          NavigationDestination(icon: Icon(Icons.graphic_eq), label: "stats")
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
     );
   }
 }
